cf5085afa17da1dbfe23381b47f25842325f51ed
Rovshan <raf-445@mail.ru>
2023-01-04 13:48:48 +0300

    connect BD

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module example.com/m/v2
 go 1.18
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
--- main.go
+++ main.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"database/sql"
-	"example.com/m/v2/service"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"html/template"
 	"log"
 	"net/http"
 )
 
 type Users struct {
-	id      int
-	login   string
-	surname string
-	name    string
-	role    int
+	Id      int
+	Login   string
+	Surname string
+	Name    string
+	Role    int
 }
 
 var database *sql.DB
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 
-	rows, err := database.Query("select * from productdb.Products")
+	rows, err := database.Query("select * from toy_shop.users")
 	if err != nil {
 		log.Println(err)
 	}
@@ -30,7 +30,7 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 
 	for rows.Next() {
 		p := Users{}
-		err := rows.Scan(&p.id, &p.login, &p.surname, &p.name, &p.role)
+		err := rows.Scan(&p.Id, &p.Login, &p.Surname, &p.Name, &p.Role)
 		if err != nil {
 			fmt.Println(err)
 			continue
@@ -56,25 +56,25 @@ func main() {
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":8181", nil)
 
-	var inputNumber int
-
-	for {
-		fmt.Println("1. Add cart\n2. Add cart item to cart\n3. Remove item from cart\n4. View cart")
-		fmt.Scanf("%d\n", &inputNumber)
-
-		switch inputNumber {
-		case 1:
-			service.AddCart()
-		case 2:
-			service.AddCartItem()
-		case 3:
-			service.DeleteCartItem()
-		case 4:
-			service.ShowCartItems()
-		case 5:
-			service.ShowCartItems()
-		default:
-			fmt.Println("This case is not exist")
-		}
-	}
+	//var inputNumber int
+	//
+	//for {
+	//	fmt.Println("1. Add cart\n2. Add cart item to cart\n3. Remove item from cart\n4. View cart")
+	//	fmt.Scanf("%d\n", &inputNumber)
+	//
+	//	switch inputNumber {
+	//	case 1:
+	//		service.AddCart()
+	//	case 2:
+	//		service.AddCartItem()
+	//	case 3:
+	//		service.DeleteCartItem()
+	//	case 4:
+	//		service.ShowCartItems()
+	//	case 5:
+	//		service.ShowCartItems()
+	//	default:
+	//		fmt.Println("This case is not exist")
+	//	}
+	//}
 }
